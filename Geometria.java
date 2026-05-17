655a8f52fcbe66fa4079bda339cd7a9040dca2ae
AF0777 <asistproyect@gmail.com>
2026-05-16 20:58:44 -0500

    feat: agregar funcion para calcular area de un triangulo

--- Geometria.java
+++ Geometria.java
@@ -4,8 +4,16 @@ public class Geometria {
         return base * altura;
     }
 
+    // Función 2: Calcular el área de un triángulo (Agregada en la segunda rama)
+    public static double calcularAreaTriangulo(double base, double altura) {
+        return (base * altura) / 2;
+    }
+
     public static void main(String[] args) {
         double areaR = calcularAreaRectangulo(5.0, 3.0);
         System.out.println("El área del rectángulo es: " + areaR);
+
+        double areaT = calcularAreaTriangulo(4.0, 6.0);
+        System.out.println("El área del triángulo es: " + areaT);
     }
 }
